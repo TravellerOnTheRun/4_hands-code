e3cdb16c33663788a1a225fc12ac9e4b37f946bb
TravellerOnTheRun <47615361+TravellerOnTheRun@users.noreply.github.com>
2020-02-19 13:43:12 +0700

    I've created the Slider component

--- src/components/Pages/Home/DemoServices/Slider/Slider.js
+++ src/components/Pages/Home/DemoServices/Slider/Slider.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import Button from '../../../../Reusables/Button/Button';
 
 const Slider = props => {
+    const [slides, setSlides] = useState([
+        'default',
+        'Custom Design',
+        'Content Creation',
+        'Copywriting',
+        'SEO',
+        'Project Management',
+        'Testing and Training',
+        'Consultations'
+    ]);
+    const [curSlide, setCurSlide] = useState(0);
+
+    useEffect(() => {
+        props.onChangeSlide(slides[curSlide]);
+    }, [curSlide]);
+
+    const nextSlide = () => {
+        if(curSlide === slides.length - 1) {
+            setCurSlide(0);
+            return;
+        };
+        setCurSlide(curSlide + 1);
+    };
+    const previousSlide = () => {
+        if(curSlide === 0) {
+            setCurSlide(slides.length -1);
+            return;
+        }
+        setCurSlide(curSlide - 1);
+    };
+
     return (
         <div className='slider'>
-
+            <Button ownStyle='btn-next' clicked={nextSlide}>Next</Button>
+            {props.children}
+            <Button ownStyle='btn-previous' clicked={previousSlide}>Previous</Button>
         </div>
     );
 };
