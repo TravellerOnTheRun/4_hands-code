e5a3cf6cb72825744fe0b17e220107ed6dfff24f
Alexander <alexivchenko9@gmail.com>
2020-02-21 09:48:39 +0700

    maybe I forgot to add smth

--- src/components/Pages/Home/DemoServices/Slider/Slider.js
+++ src/components/Pages/Home/DemoServices/Slider/Slider.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Button from '../../../../Reusables/Button/Button';
 
 const Slider = props => {
-    const [slides, setSlides] = useState([
+    const [slides] = useState([
         'default',
         'Custom Design',
         'Content Creation',
